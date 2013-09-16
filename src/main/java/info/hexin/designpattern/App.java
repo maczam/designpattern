0a0c931ef1fd9f556a21c2b360b3034b2642a3ae
maczam <maczam@163.com>
2013-09-16 17:56:10 +0800

    init

---
+++ src/main/java/info/hexin/designpattern/App.java
@@ -0,0 +1,13 @@
+package info.hexin.designpattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
