bc5d8e9e99c167d886200b8e70a76a7ac0a746c1
maczam <maczam@163.com>
2013-09-27 14:40:07 +0800

    修改备忘录模式md

--- src/main/java/info/hexin/designpattern/behavioral/memento/narrow/NarrowMemento.java
+++ src/main/java/info/hexin/designpattern/behavioral/memento/narrow/NarrowMemento.java
@@ -1,57 +1,57 @@
 package info.hexin.designpattern.behavioral.memento.narrow;
 
 public interface NarrowMemento {
-	public void narrowMethod();
+    public void narrowMethod();
 }
 
 class Originator {
-	private String state;
-	private NarrowMemento memento;
+    private String state;
+    private NarrowMemento memento;
 
-	public Originator() {
-	}
+    public Originator() {
+    }
 
-	public NarrowMemento createMemento() {
-		memento = new Memento(this.state);
-		return memento;
-	}
+    public NarrowMemento createMemento() {
+        memento = new Memento(this.state);
+        return memento;
+    }
 
-	public void restoreMemento(NarrowMemento memento) {
-		Memento aMemento = (Memento) memento;
-		this.setState(aMemento.getState());
-	}
+    public void restoreMemento(NarrowMemento memento) {
+        Memento aMemento = (Memento) memento;
+        this.setState(aMemento.getState());
+    }
 
-	public String getState() {
-		return this.state;
-	}
+    public String getState() {
+        return this.state;
+    }
 
-	public void setState(String state) {
-		this.state = state;
-	}
+    public void setState(String state) {
+        this.state = state;
+    }
 
-	// 内部类
-	protected class Memento implements NarrowMemento {
-		private String savedState;
+    // 内部类
+    protected class Memento implements NarrowMemento {
+        private String savedState;
 
-		private Memento(String someState) {
-			savedState = someState;
-		}
+        private Memento(String someState) {
+            this.savedState = someState;
+        }
 
-		private void setState(String someState) {
-			savedState = someState;
-		}
+        protected void setState(String someState) {
+            this.savedState = someState;
+        }
 
-		private String getState() {
-			return savedState;
-		}
+        protected String getState() {
+            return savedState;
+        }
 
-		public void narrowMethod() {
-			System.out.println("this is narrow method");
-		}
+        public void narrowMethod() {
+            System.out.println("this is narrow method");
+        }
 
-	}
+    }
 
-	public NarrowMemento getNarrowMemento() {
-		return memento;
-	}
+    public NarrowMemento getNarrowMemento() {
+        return memento;
+    }
 }
